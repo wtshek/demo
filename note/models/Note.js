6a02a244306ac2b9d48a65aadf6531531f1d3423
shek119 <tungtung_1999@yahoo.com.hk>
2019-12-03 15:59:12 +0100

    new Project added

--- note/models/Note.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-
-const noteSchema = new Schema({
-    title: String,
-    date: String,
-    note: String,
-})
-mongoose.model("note", noteSchema)
