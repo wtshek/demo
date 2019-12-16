179ddd57edf176ff8e896604ff79d6fd2e2c6910
shek119 <tungtung_1999@yahoo.com.hk>
2019-12-16 08:00:18 +0100

    feature work animation finished

--- Anakin_copy/index.js
+++ Anakin_copy/index.js
@@ -68,6 +68,7 @@ window.addEventListener("scroll",()=>{
     })
     rangeDetection(".footer--logo", ()=>{
         feedScence.remove()
+        console.log("feedScence removed")
     })
     rangeDetection(".footer--logo", ()=>{
         sliceScence.remove()
@@ -97,14 +98,14 @@ const progressBarAnim = (ele) => {
 const opacityAnim = (ele) => {
     gsap.fromTo(ele, {opacity:0}, {opacity:1, duration:1})
 }
+const logoArr = document.querySelectorAll(".feature_work--logo");
+const btArr = document.querySelectorAll(".feature_work--bt")
+const detailsArr = document.querySelectorAll(".feature_work--detail")
+const progressArr = document.querySelectorAll(".progress")
 
 const feature_workOnClick = (ele) => {
     const reg = /work--*/
     const arr = ele.classList
-    const logoArr = document.querySelectorAll(".feature_work--logo");
-    const btArr = document.querySelectorAll(".feature_work--bt")
-    const detailsArr = document.querySelectorAll(".feature_work--detail")
-    const progressArr = document.querySelectorAll(".progress")
 
     arr.forEach(i => {
         if (i.match(reg)){
@@ -134,3 +135,31 @@ const feature_workOnClick = (ele) => {
     })
 
 }
+
+const activeAutoChange = (arr, animFunc) => {
+    let activeIndex;
+    arr.forEach((ele, i) => {
+        if(ele.classList.contains("active")){
+            activeIndex = i
+            removeActive(ele)
+        }
+    })
+    
+    if(activeIndex+1>arr.length-1){
+        activeIndex = 0
+    } else{
+        activeIndex ++; 
+    }
+    arr[activeIndex].classList.add("active")
+
+    if(animFunc){
+        animFunc(arr[activeIndex])
+    }
+}
+
+setInterval(()=> {
+    activeAutoChange(btArr)
+    activeAutoChange(logoArr, opacityAnim)
+    activeAutoChange(detailsArr)
+    activeAutoChange(progressArr, progressBarAnim)
+}, 3000)
